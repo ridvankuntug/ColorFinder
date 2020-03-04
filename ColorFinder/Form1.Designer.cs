781b7e86ad0dd1faa2d35e85431a6792aa8defcc
ridvankuntug <ridvankuntug@gmail.com>
2020-03-04 22:27:04 +0300

    Kod tarafı Türkçeleştirildi

--- ColorFinder/Form1.Designer.cs
+++ ColorFinder/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnSec = new System.Windows.Forms.Button();
             this.btnUygula = new System.Windows.Forms.Button();
@@ -148,8 +149,9 @@
             this.Controls.Add(this.btnUygula);
             this.Controls.Add(this.btnSec);
             this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Renk Bulucu";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tbTolerans)).EndInit();
             this.ResumeLayout(false);
